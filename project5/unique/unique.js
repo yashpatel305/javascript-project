7c02b233bf308cf63b6d7170ecc6da4dbfec0740
yash patel <139000963+yashpatel305@users.noreply.github.com>
2025-04-04 10:26:34 +0530

    unique.js

--- project5/unique/unique.js
+++ project5/unique/unique.js
@@ -1,15 +1,15 @@
 // => find unique element from array
-let arr = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
-let unique = [];
-arr.forEach((item) => {
-    c=0;
-    unique.forEach((i)=>{
-        if(item===i){
+let arr = [1,1,3,4,88,7,7,8]
+let ans = []
+for (let i = 0; i < arr.length; i++) {
+    let c = 0;
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[i] === arr[j]) {
             c++;
         }
-    });
-    if(c===0){
-        unique.push(item);
     }
-});
-console.log(unique);
+    if (c === 1) {
+        ans.push(arr[i]);
+    }
+}
+console.log(ans);
